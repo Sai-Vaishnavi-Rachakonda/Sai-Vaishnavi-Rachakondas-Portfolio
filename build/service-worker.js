54a25ed20cfbff8b8a2757fc3e5247f1cbc18643
Sai-Vaishnavi-Rachakonda <111935118+Sai-Vaishnavi-Rachakonda@users.noreply.github.com>
2024-10-06 10:34:37 -0700

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -15,9 +15,7 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-importScripts(
-  "/Sai-Vaishnavi-Rachakondas-portfolio/precache-manifest.549f794c7f2cf8f242ffcf592ff33f66.js"
-);
+importScripts("/precache-manifest.30a7a10d537caad2e45c5f6007d6ecaf.js");
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -36,9 +34,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL(
-    "/Sai-Vaishnavi-Rachakondas-portfolio/index.html"
-  ),
+  workbox.precaching.getCacheKeyForURL("/index.html"),
   {
     blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
   }
